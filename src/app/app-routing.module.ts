cea4ed65ed2a32a1c7db6cfaeef1e6165edd8231
muchallen <muchallen@gmail.com>
2021-06-17 11:40:30 +0200

    my first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import {HomeComponent} from './home/home.component'
+import { ShopComponent } from './shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component : HomeComponent},
+  {path: 'shop', component : ShopComponent},
+  {path: 'checkout', component : CheckoutComponent},
+  {path: 'cart', component : CartComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { ItemComponent } from './item/item.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShopComponent,
+    ItemComponent,
+    CartComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
