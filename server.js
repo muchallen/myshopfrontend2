aae08b42f235252d33bdcb757bbb12e666e069ed
muchallen <muchallen@gmail.com>
2021-07-01 16:21:55 +0200

    First Heroku Commit

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const path = require('path')
 const app = express();
 
  
-const app = express();
- 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/myshop'));
