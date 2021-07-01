625910f52b7b0438f0b863d975bdf7fe7b7b7cf4
muchallen <muchallen@gmail.com>
2021-07-01 16:04:12 +0200

    First Heroku Commit

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/myshopdeploy'));
+app.use(express.static(__dirname + '/dist/myshop'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/myshopdeploy/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/myshop/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
