64981ac1837d61506d05367a9d027e7f766c2b7c
muchallen <muchallen@gmail.com>
2021-07-08 16:10:12 +0200

    package json file7

--- api/serve.js
+++ api/serve.js
@@ -2,42 +2,42 @@
 module.exports = (req,res) => {
  // Install express server with
 const express = require('express')
-const path = require('path');
-const multer = require('multer');
-var cors = require('cors')
+// const path = require('path');
+// const multer = require('multer');
+// var cors = require('cors')
 const app = express();
 
-app.use(cors({
-  origin: '*'
-}));
+// app.use(cors({
+//   origin: '*'
+// }));
 
 
 
-var storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-      cb(null, './src/assets/images/productsimages')
-  },
-  filename: (req, file, cb) => {
-      cb(null, '' + Date.now()+file.originalname)
-  }
-});
+// var storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//       cb(null, './src/assets/images/productsimages')
+//   },
+//   filename: (req, file, cb) => {
+//       cb(null, '' + Date.now()+file.originalname)
+//   }
+// });
 
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 
-// Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/myshop'));
-//app.use(express.static('public'));
-const upload = multer({ storage: storage });
+// // Serve only the static files form the dist directory
+// // Replace the '/dist/<to_your_project_name>'
+// app.use(express.static(__dirname + '/dist/myshop'));
+// //app.use(express.static('public'));
+// const upload = multer({ storage: storage });
 
-app.use('/ftp', express.static('public'));
+// app.use('/ftp', express.static('public'));
 
-app.post('/uploadImage', upload.single('file'), function(req,res) {
-  console.log(req.file);
-  console.log('storage location is ', req.hostname +'/' + req.file.path);
-  res.send({name:req.file.filename});
-  return
-})
+// app.post('/uploadImage', upload.single('file'), function(req,res) {
+//   console.log(req.file);
+//   console.log('storage location is ', req.hostname +'/' + req.file.path);
+//   res.send({name:req.file.filename});
+//   return
+// })
 
 const port = process.env.PORT || 4200;
 app.listen(port, () => {
