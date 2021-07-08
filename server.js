b040f93e27d1a097786148d282f366dc3ee50ed8
muchallen <muchallen@gmail.com>
2021-07-08 12:21:09 +0200

    package json file

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const path = require('path');
 const multer = require('multer');
 var cors = require('cors')
 const app = express();
+
 app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
