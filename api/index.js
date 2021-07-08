26f3f85b18d4584cbac98d63c1463223362275eb
muchallen <muchallen@gmail.com>
2021-07-08 17:59:54 +0200

    api correction

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ const upload = multer({ storage: storage });
 
 app.use('/ftp', express.static('public'));
 
-app.post('/api/uploadImage', upload.single('file'), function(req,res) {
+app.post('api/uploadImage', upload.single('file'), function(req,res) {
   console.log(req.file);
   console.log('storage location is ', req.hostname +'/' + req.file.path);
   res.send({name:req.file.filename});
@@ -45,9 +45,14 @@ app.listen(port, () => {
 
  
  
-app.get('/api/allen', function(req,res) {
+app.get('api/allen', function(req,res) {
   res.send('allen')
 });
+
+app.get('api', function(req,res) {
+  res.send('showing api')
+});
+
 
 
 module.exports = app;
