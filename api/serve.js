2dffc7c403b6b03f3f1becf5138c01fc960df763
muchallen <muchallen@gmail.com>
2021-07-08 15:49:43 +0200

    package json file7

--- api/serve.js
+++ api/serve.js
@@ -1,4 +1,32 @@
-module.exports = (req, res) => {
-    const { name = 'World' } = req.query;
-    res.status(200).send(`Hello ${name}!`);
+
+module.exports = (req,res) => {
+    // Install express server with
+const express = require('express')
+const path = require('path');
+const multer = require('multer');
+var cors = require('cors')
+const app = express();
+
+app.use(cors({
+  origin: '*'
+}));
+
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, './src/assets/images/productsimages')
+  },
+  filename: (req, file, cb) => {
+      cb(null, '' + Date.now()+file.originalname)
+  }
+});
+
+app.use(express.urlencoded({ extended: false }));
+
+const upload = multer({ storage: storage });
+
+  console.log(req.file);
+  console.log('storage location is ', req.hostname +'/' + req.file.path);
+  res.send({name:req.file.filename});
+  return
+
   };
