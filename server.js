d38c2e3197a500ff5d85fcde9f06445b7b148ceb
muchallen <muchallen@gmail.com>
2021-07-08 10:31:16 +0200

    images upload

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require('express')
 const path = require('path');
 const multer = require('multer');
-const serveIndex = require('serve-index')
 var cors = require('cors')
 const app = express();
 app.use(cors())
