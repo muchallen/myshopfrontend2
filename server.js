95328c1d35aa2be62867531b47fd171005992145
muchallen <muchallen@gmail.com>
2021-07-08 12:37:52 +0200

    package json file

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ const multer = require('multer');
 var cors = require('cors')
 const app = express();
 
-app.use(cors())
-var corsOptions = {
-  origin: 'https://centric-shop-backend.herokuapp.com',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+app.use(cors({
+  origin: '*'
+}));
 
 
 
