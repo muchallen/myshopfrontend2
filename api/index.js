70e9d3ed8f171ab32d21daefe02e826ec94eedfa
muchallen <muchallen@gmail.com>
2021-07-09 11:45:23 +0200

    serverless functions

--- api/index.js
+++ api/index.js
@@ -1,24 +1,24 @@
 // Install express server with
 const express = require('express')
-// const path = require('path');
-// const multer = require('multer');
-// var cors = require('cors')
+const path = require('path');
+const multer = require('multer');
+var cors = require('cors')
 const app = express();
 
-// app.use(cors({
-//   origin: '*'
-// }));
+app.use(cors({
+  origin: '*'
+}));
 
 
 
-// var storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//       cb(null, '../src/assets/images/productsimages')
-//   },
-//   filename: (req, file, cb) => {
-//       cb(null, '' + Date.now()+file.originalname)
-//   }
-// });
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, '../src/assets/images/productsimages')
+  },
+  filename: (req, file, cb) => {
+      cb(null, '' + Date.now()+file.originalname)
+  }
+});
 
 // app.use(express.urlencoded({ extended: false }));
 
@@ -26,16 +26,16 @@ const app = express();
 // // Replace the '/dist/<to_your_project_name>'
 // app.use(express.static(__dirname + '/dist/myshop'));
 // //app.use(express.static('public'));
-// const upload = multer({ storage: storage });
+const upload = multer({ storage: storage });
 
 // app.use('/ftp', express.static('public'));
 
-// app.post('api/uploadImage', upload.single('file'), function(req,res) {
-//   console.log(req.file);
-//   console.log('storage location is ', req.hostname +'/' + req.file.path);
-//   res.send({name:req.file.filename});
-//   return
-// })
+app.post('api/uploadImage', upload.single('file'), function(req,res) {
+  console.log(req.file);
+  console.log('storage location is ', req.hostname +'/' + req.file.path);
+  res.send({name:req.file.filename});
+  return
+})
 
 // const port = process.env.PORT || 4200;
 // app.listen(port, () => {
